47cd6d8f1dda166916df844e1a62ab4093796e23
Phillipe Rodriguez <philliperod@gmail.com>
2021-01-01 16:02:23 -0600

    created a conditional statement with an array_filter; if error, it will return an error statement; if not then it will redirect to the home page

--- add.php
+++ add.php
@@ -7,22 +7,21 @@
 
     
 
-    // THROWING ERRORS
+    // REDIRECTING
+
+    // if there are no errors from the form fields then we want to redirect to the home page
+    // you'll need to use a conditional statement to check if any errors exist
+    // to do so, you will need to check the $error_array if there are any empty strings
+    // if not then redirect to the home page
 
     $email = $title = $ingredients = '';
-    // shorthand to assign variables to the same value
-
     $error = [
         'email' => '',
         'title' => '',
         'ingredients' => ''
     ];
-    // you can create an array for your errors
-    // to keep the inputs given by the user in its field, we will need to initialize an empty string for each variable that was created previously 
-    // then you will set a key attribute value into <input> so once a field is submitted it will retain that input
 
     if(isset($_POST['submit'])){
-        
         if(empty($_POST['email'])){
             $error['email'] = 'An email is required <br/>';
         } else {
@@ -47,11 +46,23 @@
             $ingredients = $_POST['ingredients'];
             if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
                 $error['ingredients'] = 'Ingredients must be comma separated list only <br \>';
-        }
-    } 
+            }
+        } 
     }
     
 
+    if(array_filter($error)){
+        // array_filter - filters elements of an array using a callback function
+        // iterates over each value in the array passing them to the callback function 
+        // If the callback function returns TRUE, the current value from array is returned into the result array
+        echo 'Errors in the form';
+    } else {
+        echo 'Form is valid';
+        header('Location: index.php');
+        // header() - is used to send a raw HTTP header
+        // must be called before any actual output is sent, either by normal HTML tags, blank lines in a file, or from PHP
+        // very common error to read code with include, or require, functions, or another file access function, and have spaces or empty lines that are output before header() is called
+    }
 ?>
 
 <!DOCTYPE html>
@@ -63,15 +74,12 @@
     <h4 class="center">Add a Pizza</h4>
     <form action="add.php" method="POST" class="white">
         <input type="email" name="email" class="validate" value="<?php echo htmlspecialchars($email) ?>">
-        <!-- created a value key attribute that will retain user's input in the field -->
         <label for="email">Your Email: </label>
         <div class="red-text"><?php echo $error['email']; ?></div>
         <input type="text" name="title" class="validate" value="<?php echo htmlspecialchars($title) ?>">
-        <!-- created a value key attribute that will retain user's input in the field -->
         <label for="title">Pizza Title: </label>
         <div class="red-text"><?php echo $error['title']; ?></div>
         <input type="text" name="ingredients" class="validate" value="<?php echo htmlspecialchars($ingredients) ?>">
-        <!-- created a value key attribute that will retain user's input in the field -->
         <label for="ingredients">Ingredients (comma separated): </label>
         <div class="red-text"><?php echo $error['ingredients']; ?></div>
         <br>
