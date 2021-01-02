387e7e182dcfa7ece2f7b3a591fd2747c99db566
Phillipe Rodriguez <philliperod@gmail.com>
2021-01-02 17:29:20 -0600

    created explode function to return an array of split strings

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-    // RENDERING DATA TO THE BROWSER
-    // attempting to output to a template on the browser
-    // will output $pizzas
+    // EXPLODE FUNCTION
 
     $connect = mysqli_connect('localhost', 'phil', 'test1', 'php_pizza');
 
@@ -16,6 +14,12 @@
 
     mysqli_free_result($result);
     mysqli_close($connect);
+    explode(',', $pizzas[0]['ingredients']);
+    // explode(separator, string)
+    // returns an array of strings created by splitting the STRING parameter on boundaries formed by the SEPARATOR
+    // first parameter looks for the specified character then looks for the value before that
+    // takes that and puts it in its own position in the array
+    // basically this is how we want to split up the string in the array
 
 ?>
 
@@ -34,7 +38,12 @@
             <div class="card z-depth-0">
                 <div class="card-content center">
                     <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-                    <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                    <ul>
+                        <?php foreach (explode(',', $pizza['ingredients']) as $ingredient) { ?>
+                        <li><?php echo htmlspecialchars($ingredient); ?></li>
+                        <?php }?>
+                        <!-- this is the second argument that will split up the string itself and place each value in its own line item -->
+                    </ul>
                 </div>
                 <div class="card-action right-align">
                     <a href="#" class="brand-text">More Info</a>
