177381ef87e4a5e8ee37fe86f627005ea6d793fa
Phillipe Rodriguez <philliperod@gmail.com>
2021-01-04 10:16:19 -0600

    structured out the html page; created a conditional that displays info about the pizza or states no pizza exists if one is not found in the table

--- details.php
+++ details.php
@@ -2,41 +2,16 @@
 
     // OBTAINING A SINGLE RECORD
 
-    // Step 1 - in index.php, use the <a> tag to reference this page
-    // <a href="details.php?id=<?php echo $pizza['id']; >
-    // created a query string
-    // this will be a GET request that looks for a specific id
-    // basically want to access the individual data and link it to the url
-    //
-
-    // Step 2 - check GET request id parameter
-    // localhost:8888/details.php?id={id_number}
-    // id={id_number} looks exactly like the GET request
-
     include 'config/db_connect.php';
 
     if (isset($_GET['id'])) {
-        // isset() - checking if the id has been set
         $id = mysqli_real_escape_string($connect, $_GET['id']);
-        //mysqli_real_escape_string() - escapes special characters in a string for use in an SQL statement, taking into account the current charset of the connection
-
-        // Step 3 - make SQL query
-
         $sql = "SELECT * FROM pizzas WHERE id = {$id}";
-        // must be in double quotes
-
-        // Step 4 - get query result
-
         $result = mysqli_query($connect, $sql);
-
-        // Step 5 - fetch result in array format
-
         $pizza = mysqli_fetch_assoc($result);
 
         mysqli_free_result($result);
         mysqli_close($connect);
-
-        print_r($pizza);
     }
 
 ?>
@@ -46,7 +21,17 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<h2>Details</h2>
+<div class="container center">
+    <?php if ($pizza) { ?>
+    <h4><?php echo htmlspecialchars($pizza['title']); ?></h4>
+    <p>Created by: <?php echo htmlspecialchars($pizza['email']); ?></p>
+    <p>Timestamp: <?php echo date($pizza['created_at']); ?></p>
+    <h5>Ingredients:</h5>
+    <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+    <?php } else { ?>
+    <h4>This pizza does not exists</h4>
+    <?php } ?>
+</div>
 
 </html>
 
