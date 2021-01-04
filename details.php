74a9af0ad5545dc3c9736aa08244b148fa3de41a
Phillipe Rodriguez <philliperod@gmail.com>
2021-01-04 15:07:55 -0600

    created an if block that checks the POST request for the delete parameter, remove it from the database, and redirect you back to index.php

--- details.php
+++ details.php
@@ -1,14 +1,35 @@
 <?php
 
 // DELETE A RECORD
-// to achieve this, we will need to create a form in html with a hidden input that stores the id of the pizza in key attribute 'value' with a delete button
+// to achieve this, we will need to create a form in html
+// with a hidden input that stores the id of the pizza in key attribute 'value' with a delete button
 // a submit button will then make a POST request and take that value from the input field into php
 // create a conditional statement that will detect if a POST request has been set
 // if true, take that id and make a request to the database to say we want to delete the id
 
     include 'config/db_connect.php';
 
-    // Step 2 - CHECK POST REQUEST ID PARAMETER
+    // Step 2 - CHECK POST REQUEST DELETE PARAMETER
+    // this will look like the GET request if block
+    // when the submission runs, it will initialize that value for it $_POST['delete']
+
+    if (isset($_POST['delete'])) {
+        $id_to_delete = mysqli_real_escape_string($connect, $_POST['id_to_delete']);
+        // this name is obtained from the hidden input in the form
+        // which will have the value of id for that record
+
+        // make sql query
+
+        $sql = "DELETE FROM pizzas WHERE id = {$id_to_delete}";
+
+        if (mysqli_query($connect, $sql)) {
+            // if successful, change the direction for the user
+            header('Location: index.php');
+        } else {
+            // failure
+            echo 'Query error: '.mysqli_error($connect);
+        }
+    }
 
     if (isset($_GET['id'])) {
         $id = mysqli_real_escape_string($connect, $_GET['id']);
@@ -19,7 +40,6 @@
         mysqli_free_result($result);
         mysqli_close($connect);
     }
-
 ?>
 
 <?php include 'templates/header.php'; ?>
@@ -35,19 +55,8 @@
     <h5>Ingredients:</h5>
     <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
 
-    <!-- Step 1 - DELETE FORM -->
-
     <form action="details.php" method="POST">
-        <!-- 
-            // action - is the file we want to run when the submission is made 
-            // method - send this data; when submitted then we need to detect that with a conditional statement
-        -->
         <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
-        <!-- 
-            // type - will be hidden so it will not be seen on the page
-            // name - the id created to delete the record from the database
-            // value - will equal to the id in the database that was obtained from the GET request to delete
-        -->
         <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
     </form>
 
